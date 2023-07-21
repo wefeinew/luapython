3da747d366e3931eb66f84b743ace2607b2ab7e2
Droped <124509814+wefeinew@users.noreply.github.com>
2023-07-21 21:38:51 +0500

    Update luapython.py

--- luapython.py
+++ luapython.py
@@ -1,28 +1,57 @@
 import subprocess
 import os
+import requests
+from bs4 import BeautifulSoup
 
-print("LIB: Lua Python. By wefeinew Github and source file: https://github.com/wefeinew/luapython")
+print("LIB: Lua Python. By Pythonlua.org")
 
 def run_lua_script(lua_cmd, *args):
     result = subprocess.run(lua_cmd + list(args), capture_output=True, text=True, input=None if not args else '\n'.join(args))
     if result.returncode == 0:
         output = result.stdout.strip()
-        print(output)
+        print(output)  # Вывод результата в консоль
         return output
     else:
         error_message = result.stderr.strip() if result.stderr else "An error occurred while executing the Lua script."
         raise RuntimeError(error_message)
+
+def search_luarocks(query):
+    url = f"https://luarocks.org/search?q={query}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        modules = soup.find_all("a", class_="module-link")
+
+        if modules:
+            module = modules[0]
+            module_name = module.text.strip()
+            module_url = module["href"]
+
+            download_url = f"https://luarocks.org{module_url}/download"
+            module_response = requests.get(download_url)
+
+            if module_response.status_code == 200:
+                with open(f"{module_name}.rock", "wb") as file:
+                    file.write(module_response.content)
+                print(f"Module '{module_name}' successfully downloaded")
+            else:
+                print("An error occurred while downloading the module")
+        else:
+            print("Modules not found")
+    else:
+        print("An error occurred while executing the request")
 
 def lua_terminal():
     lua_cmd = ['Lua/lua.exe']
     process = subprocess.Popen(lua_cmd)
 
 def get_lua_version():
-    lua_cmd = ['Lua/lua.exe', '-vef']
+    lua_cmd = ['Lua/lua.exe', '-v']
     process = subprocess.Popen(lua_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
-    output, error = process.communicate()
+    output, error = process.communicate()  # Дождаться завершения процесса и получить вывод
     
-    if process.returncode != 0: 
+    if process.returncode != 0:  # Проверить код возврата процесса на наличие ошибок
         error_message = error.strip() if error else "An error occurred while executing the Lua"
         raise RuntimeError(error_message)
     
