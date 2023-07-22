568ac7ffff1545c0c38666442b7ff37a15feec54
Droped <124509814+wefeinew@users.noreply.github.com>
2023-07-22 14:08:06 +0500

    Update Luacpp.cpp

--- Luacpp.cpp
+++ Luacpp.cpp
@@ -2,18 +2,15 @@
 #include <lua.hpp>
 
 int main() {
-    // Инициализация интерпретатора Lua
+    
     lua_State* L = luaL_newstate();
     
-    // Загрузка стандартных библиотек Lua
     luaL_openlibs(L);
-
-    // Загрузка и выполнение скрипта Lua из файла
-    if (luaL_dofile(L, "script.lua") != LUA_OK) {
-        std::cerr << "Ошибка выполнения скрипта: " << lua_tostring(L, -1) << std::endl;
-    }
     
-    // Освобождение ресурсов интерпретатора Lua
+    if (luaL_dofile(L, "script.lua") != LUA_OK) {
+        std::cerr << "Script execution error: " << lua_tostring(L, -1) << std::endl;
+    }
+
     lua_close(L);
 
     return 0;
