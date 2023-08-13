2702c6a741eb89d3b8d08217fab0c8716f40f165
Droped <124509814+wefeinew@users.noreply.github.com>
2023-08-13 18:35:14 +0500

    Update lua_script_util.py

--- lua_script_util.py
+++ lua_script_util.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 import requests
-import lupa
 from bs4 import BeautifulSoup
 
 print("LIB: Lua Script Util. Version: 10.12.31")
@@ -22,13 +21,17 @@ def search_luarocks(query):
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
-        modules = soup.find_all("a", class_="module-link")
+        modules = soup.find_all("div", class_="module")
 
         if modules:
             module = modules[0]
-            module_name = module.text.strip()
-            module_url = module["href"]
+            module_name = module.find("h2").text.strip()
+            module_desc = module.find("p", class_="module-description").text.strip()
 
+            print(f"Module: {module_name}")
+            print(f"Description: {module_desc}")
+
+            module_url = module.find("a", class_="module-link")["href"]
             download_url = f"https://luarocks.org{module_url}/download"
             module_response = requests.get(download_url)
 
@@ -42,21 +45,20 @@ def search_luarocks(query):
             print("Modules not found")
     else:
         print("An error occurred while executing the request")
-
+        
 def lua_terminal():
     lua_cmd = ['Lua/lua.exe']
     process = subprocess.Popen(lua_cmd)
 
 def get_lua_version():
     lua_cmd = ['Lua/lua.exe', '-v']
-    process = subprocess.Popen(lua_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
-    output, error = process.communicate() 
-    
-    if process.returncode != 0:
-        error_message = error.strip() if error else "An error occurred while executing the Lua"
+    result = subprocess.run(lua_cmd, capture_output=True, text=True)
+    if result.returncode == 0:
+        output = result.stdout.strip()
+        return output
+    else:
+        error_message = result.stderr.strip() if result.stderr else "An error occurred while getting Lua version."
         raise RuntimeError(error_message)
-    
-    return output.strip()
 
 lua_version = get_lua_version()
 print(lua_version)
@@ -68,7 +70,7 @@ def luacreate(script_name):
         if confirm.lower() != "y":
             return
     with open(script_path, "w") as f:
-        f.write("")
+        f.write("-- Your Lua script here")
     print(f"Created file: {script_path}")
 
 def luarun(script_path, *args):
